a9208ba90ed24a5674f1c5325be13068a3eedd83
fabioarduin <fabioarduin@gmail.com>
2022-04-11 14:29:43 -0300

    Create atividade individual.js

---
+++ atividade individual.js
@@ -0,0 +1,20 @@
+function sum(){
+    let num1 = 5;
+    let num2 = 10;
+    let soma = num1 + num2;
+    console.log(soma)
+}
+
+console.log(sum())
+
+function sub(){
+    //TODO: Implementar subtração
+}
+
+function mult(){
+    //TODO: Implementar multiplicação
+}
+
+function div(){
+    //TODO: Implementar divisão aqui
+}
